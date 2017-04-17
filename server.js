b3e29b8221b5e9419c8a5847872c53d19e28bd74
Pancho <panchenko923@gmail.com>
2017-04-17 12:30:01 +0300

    test

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 
 var express = require('express'),
 bodyParser = require('body-parser'),
-app = express();
+app = express(),
+path = require('path');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
@@ -9,6 +10,8 @@ app.use(bodyParser.json());
 app.get('/', function (request, response) {
     response.render('index.ejs');
 });
+
+app.use(express.static('js'));
 
 app.listen(3000, function () {
     console.log('work on 3000 port!');
