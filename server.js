f5b755ecb415ea26d59d25c3784b7f5420846d7d
Pancho <panchenko923@gmail.com>
2017-04-17 13:57:20 +0300

    js doesn't work

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 
 var express = require('express'),
-bodyParser = require('body-parser'),
-app = express(),
-path = require('path');
+    bodyParser = require('body-parser'),
+    app = express(),
+    path = require('path');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-app.get('/', function (request, response) {
+/*app.get('/', function (request, response) {
     response.render('index.ejs');
-});
+});*/
 
-app.use(express.static('js'));
+app.get('/', function(req, res) { res.sendFile(path.join(__dirname + '/index.html')); });
+
+/*app.use(express.static('./view'));*/
 
 app.listen(3000, function () {
     console.log('work on 3000 port!');
