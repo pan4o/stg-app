5c4a8ff6d25b318c8edffe4fa6e8cc1280caf329
Pancho <panchenko923@gmail.com>
2017-04-18 22:33:16 +0300

    small fixes

--- server.js
+++ server.js
@@ -11,10 +11,7 @@ app.get('/', function(request, response) {
     response.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.use("/public", express.static(path.join(__dirname + '/public')));
-
-//Понять чем отличается от варианта с path.join (работают одинаково)
-//app.use("/public", express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(3000, function () {
     console.log('work on 3000 port!');
